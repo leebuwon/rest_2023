4d05ec2465e04417548e87eff4e8be91f5521450
bw1611 <bw1611@naver.com>
2023-05-17 13:16:50 +0900

    init commit

---
+++ src/main/java/com/ll/rest_2023/Rest2023Application.java
@@ -0,0 +1,13 @@
+package com.ll.rest_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rest2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rest2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/rest_2023/Rest2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.rest_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rest2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

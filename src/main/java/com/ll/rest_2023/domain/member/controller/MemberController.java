3effc0ff9f6c6a10855c19b38dbdca2aab3b0ab7
bw1611 <bw1611@naver.com>
2023-05-17 14:21:53 +0900

    feat: login TestCase / SecurityConfig 추가

---
+++ src/main/java/com/ll/rest_2023/domain/member/controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.ll.rest_2023.domain.member.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    @PostMapping("/login")
+    public String login() {
+        return "성공";
+    }
+}
